374331140610d53306b04e2b4513b48d4e4aa10c
SKurbatova <58175476+SofiFairyTell@users.noreply.github.com>
2023-03-20 21:39:45 +0300

    Update ColleratorPro.cs

--- ColleratorPro.cs
+++ ColleratorPro.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace CorrelatorPro
 {
@@ -66,11 +67,125 @@ namespace CorrelatorPro
             Yi2 = SquareArrayElements(Yi2);
 
             WriteToDataGridView(dgvTemp,Xi, Yi, XiYi, Xi2, Yi2, Xi.Length);
+
+            // Найдем средние значения
+            double avgXi = Xi.Sum() / Xi.Length;
+            double avgYi = Yi.Sum() / Yi.Length;
+            double avgXiYi = XiYi.Sum() / XiYi.Length;
+            //Найдем средние квадратичные отклонения случайных величин X и Y
+            double avgDevX = Math.Sqrt((Xi2.Sum() / Xi2.Length) - Math.Pow(avgXi, 2.0));
+            double avgDevY = Math.Sqrt((Yi2.Sum() / Yi2.Length) - Math.Pow(avgYi, 2.0));
+            //Найдем выборочный коэффициент корреляции
+            double rV = ((avgXiYi - avgXi * avgYi) / (avgDevX * avgDevY));
+            string checkrV = CheckConnect(rV);
+            //Найдем коэффициенты линейной регрессии
+            double roYX = (avgXiYi - avgXi * avgYi) / Math.Pow(avgDevX, 2);
+            double bYX = avgYi - avgXi * roYX;
+            double[] outputY = new double[Yi.Length];
+            double[] outputX = new double[Xi.Length];
+            LineRegressionOutput(out outputY, Xi, roYX, bYX);
+            double roXY = (avgXiYi - avgXi * avgYi) / Math.Pow(avgDevY, 2);
+            double bXY = avgXi - avgYi * roXY;
+            LineRegressionOutput(out outputX, Yi, roXY, bXY);
+
+        }
+
+
+        public void DrawChart(double[] X, double[] Y, double[] Y1)
+        {
+
+            // Устанавливаем размеры и местоположение Chart
+            ChartOfFunction.Size = new Size(380, 330);
+            //ChartOfFunction.Location = new Point(10, 10);
+
+            // Создаем новую область Chart
+            ChartArea chartArea1 = new ChartArea();
+            if (ChartOfFunction.ChartAreas.Count > 0)
+            {
+                ChartOfFunction.ChartAreas.Clear();
+            };
+            ChartOfFunction.ChartAreas.Add(chartArea1);
+
+            // Создаем новый объект Series для хранения точек графика
+            Series series1 = new Series();
+            Series series2 = new Series();
+            series1.ChartType = SeriesChartType.Point;
+            series2.ChartType = SeriesChartType.Line;
+            series1.Color = Color.Red;
+            series2.Color = Color.Blue;
+            series1.LegendText = "Эксперименты";
+            series2.LegendText = "Результаты аппроксимации";
+            // Добавляем точки в Series
+            for (int i = 0; i < X.Length; i++)
+            {
+                series1.Points.AddXY(X[i], Y[i]);
+                series2.Points.AddXY(X[i], Y1[i]);
+            }
+
+            if (ChartOfFunction.Series.Count > 0)
+            {
+                ChartOfFunction.Series.Clear();
+            };
+
+            // Добавляем Series в Chart
+            ChartOfFunction.Series.Add(series1);
+            ChartOfFunction.Series.Add(series2);
+        }
+
+        public static void LineRegressionOutput(out double[] output, double[] InEl, double roYX, double bYX)
+        {
+            output = new double[InEl.Length];
+            for (int i=0; i< InEl.Length;i++)
+            {
+                output[i] = (roYX * InEl[i]) - bYX;
+            }
         }
         /*Метод получения данных из таблицы
          * @param таблица DataGridView
          * @return массив Xi и Yi
          */
+        private static string CheckConnect(double rV)
+        {
+            //По шкале Чеддока
+            string answer = "";
+            answer = rV > 0 ? "Прямая" : "Обратная";
+            if ((0.99 < Math.Abs(rV))&&(Math.Abs(rV) <= 1.0))
+            {
+                answer = answer+" практически функциональная";
+                return answer;
+            };
+            if ((0.9 < Math.Abs(rV)) && (Math.Abs(rV) <= 0.99))
+            {
+                answer = answer + " очень сильная";
+                return answer;
+            };
+            if ((0.7 < Math.Abs(rV)) && (Math.Abs(rV) <= 0.9))
+            {
+                answer = answer + " сильная";
+                return answer;
+            };
+            if ((0.5 < Math.Abs(rV)) && (Math.Abs(rV) <= 0.7))
+            {
+                answer = answer + " заметная";
+                return answer;
+            };
+            if ((0.3 < Math.Abs(rV)) && (Math.Abs(rV) <= 0.5))
+            {
+                answer = answer + " умеренная";
+                return answer;
+            };
+            if ((0.1 < Math.Abs(rV)) && (Math.Abs(rV) <= 0.3))
+            {
+                answer = answer + " слабая";
+                return answer;
+            };
+            if ((0.0 < Math.Abs(rV)) && (Math.Abs(rV) <= 0.1))
+            {
+                answer = answer + " практически отсутствует";
+                return answer;
+            };
+            return answer;
+        }
 
         private void GetDataFromDataGridView(DataGridView dgv, out double[] x, out double[] y)
         {
@@ -87,7 +202,6 @@ namespace CorrelatorPro
                 y[i] = double.Parse(dgv.Rows[1].Cells[i].Value.ToString());
             }
         }
-
 
         private void WriteToDataGridView(DataGridView dgv, double[] Xi, double[] Yi, double[] XiYi, double[] XiSqr, double[] YiSqr, int size)
         {
@@ -114,9 +228,6 @@ namespace CorrelatorPro
             dgv.Rows[dgv.Rows.Count - 1].Cells[3].Value = XiSqr.Sum();
             dgv.Rows[dgv.Rows.Count - 1].Cells[4].Value = YiSqr.Sum();
         }
-
-
-
 
 
 
